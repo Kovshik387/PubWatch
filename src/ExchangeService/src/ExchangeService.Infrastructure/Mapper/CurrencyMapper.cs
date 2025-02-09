71dcd402ddb97dfd809b6f7631acae4ac436fe9e
Kovshik387 <fantokin03@mail.ru>
2025-02-09 19:12:34 +0300

    feat: add ExchangeService

---
+++ src/ExchangeService/src/ExchangeService.Infrastructure/Mapper/CurrencyMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExchangeService.Application.Data.Dto;
+using ExchangeService.Domain.Entities;
+
+namespace ExchangeService.Infrastructure.Mapper;
+
+public class CurrencyMapper : Profile
+{
+    public CurrencyMapper()
+    {
+        CreateMap<Currency, CurrencyDto>().ReverseMap();
+        CreateMap<QuotationDto, Quotation>().ReverseMap();
+    }
+}
