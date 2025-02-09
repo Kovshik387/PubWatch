d2264faa4bf06dc94999e9eebe56d9cd2d84dc4d
Kovshik387 <fantokin03@mail.ru>
2025-02-09 19:10:49 +0300

    fix: cast to class

--- src/AccountService/src/AccountService.Application/Dto/AccountDto.cs
+++ src/AccountService/src/AccountService.Application/Dto/AccountDto.cs
@@ -1,3 +1,10 @@
 ﻿namespace AccountService.Application.Dto;
 
-public record AccountDto(string Name, string Surname,string? Patronymic, IEnumerable<FavoriteDto> Favorites);
+public class AccountDto
+{
+    public string Name { get; init; } = string.Empty;
+    public string Surname { get; init; } = string.Empty;
+    public string? Patronymic { get; init; } = string.Empty;
+    public IEnumerable<FavoriteDto> Favorites { get; init; } = [];
+    public string Image { get; set; } = string.Empty;
+}
