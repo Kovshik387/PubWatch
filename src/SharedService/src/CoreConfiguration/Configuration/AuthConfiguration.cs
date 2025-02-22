ce0364ebba903e35595572af4c0a3299021850cd
Kovshik387 <fantokin03@mail.ru>
2025-02-22 20:21:50 +0300

    fix

--- src/SharedService/src/CoreConfiguration/Configuration/AuthConfiguration.cs
+++ src/SharedService/src/CoreConfiguration/Configuration/AuthConfiguration.cs
@@ -28,7 +28,6 @@ public static class AuthConfiguration
                     RequireExpirationTime = true,
                     ValidateLifetime = true,
                 };
-                options.UseSecurityTokenValidators = true;
                 options.Events = new JwtBearerEvents
                 {
                     OnChallenge = context =>
