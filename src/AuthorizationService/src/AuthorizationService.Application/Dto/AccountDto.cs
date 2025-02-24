a4bd35a2485ff1ebe62c7340d1fc3a611c2444c8
Kovshik387 <fantokin03@mail.ru>
2025-02-25 00:26:38 +0300

    feat: add accept field

--- src/AuthorizationService/src/AuthorizationService.Application/Dto/AccountDto.cs
+++ src/AuthorizationService/src/AuthorizationService.Application/Dto/AccountDto.cs
@@ -7,4 +7,5 @@ public class AccountDto
     public string SurName { get; set; } = string.Empty;
     public string Patronymic { get; set; } = string.Empty;  
     public string Email { get; set; } = string.Empty;
+    public bool Accept { get; set; }
 }
