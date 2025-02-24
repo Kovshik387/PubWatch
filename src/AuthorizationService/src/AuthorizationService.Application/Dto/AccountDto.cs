886e1530cc0f9379be90bf55109d20f4b82a2d77
Kovshik387 <fantokin03@mail.ru>
2025-02-25 00:30:51 +0300

    miss

--- src/AuthorizationService/src/AuthorizationService.Application/Dto/AccountDto.cs
+++ src/AuthorizationService/src/AuthorizationService.Application/Dto/AccountDto.cs
@@ -7,5 +7,4 @@ public class AccountDto
     public string SurName { get; set; } = string.Empty;
     public string Patronymic { get; set; } = string.Empty;  
     public string Email { get; set; } = string.Empty;
-    public bool Accept { get; set; }
 }
